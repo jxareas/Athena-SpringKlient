4ddf121b8b175b0b545c700a901f0baf31809110
jxareas <areasjx@gmail.com>
2024-07-16 15:27:16 -0600

    Initial commit

---
+++ src/main/kotlin/com/jxareas/athenaklient/AthenaKlientApplication.kt
@@ -0,0 +1,11 @@
+package com.jxareas.athenaklient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AthenaKlientApplication
+
+fun main(args: Array<String>) {
+	runApplication<AthenaKlientApplication>(*args)
+}
